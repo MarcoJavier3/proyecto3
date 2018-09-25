009dc48920eeb16cac7ab24a17cfec854ad9fb95
MarcoJavier3 <jorgevilla-10@hotmail.com>
2018-09-25 15:52:46 -0500

    version Javascript calcular iva

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,31 @@
 /*
-console.log("Hola mundo");
+function validateForm(){
 
-var numero1= 10.5;
-var numero2= 5;
-var resultado = numero1*numero2;
-console.log(resultado);
-
-var nombre = 'Marco Javier Bayona';
-
-var profesion = "Desarrollador Full Stack";
-
-console.log(nombre + profesion);
-
-var tiene_mascota = true;
-
-datos =["juan",26,false,25,78,"desarrollador"];
-console.log(datos[0]);
-
-var edad = 41 ;
-if(edad>=18 && edad<=40){
-    console.log("Eres un adulto joven");
+    var titulos = document.getElementsByTagName('h1');
+    console.log(titulos);
 }
-else
-{
-    console.log("No eres un adulto joven");
-}
-var dias = ['Lunes','Martes', 'Miercoles','Jueves','viernes','Sabado','Domingo'];
-for(i=0; i<7;i++){
-console.log(dias[i]);
-}
-*/
 
-function validateForm() {
-    var x = parseInt (document.forms["myForm"]["numero1"].value);
-    var y = parseInt( document.forms["myForm"]["numero2"].value);
-
-  
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-        if (y == "") {
-            alert("Name must be filled out");
-            return false;
+function validateForm2(){
+    
+        var clasetexto = document.getElementsByClassName('texto');
+        console.log(clasetexto);
     }
-    var iva = x*(y/100);
-    return iva;
+var sede = document.getElementById('principal');
+console.log(principal);
+*/
+function iva(precio_producto,porcentaje_iva){
+    iva_producto =precio_producto*(porcentaje_iva/100);
+    return iva_producto;
 }
+
+function calcular_iva(){
+    var precio = parseInt(document.getElementById("precio").value);
+    var porcentaje_iva = parseInt(document.getElementById("iva").value);
+    var resultado= iva(precio,porcentaje_iva);
+    var salida = document.getElementById('resultado');
+    /*var texto =document.createTextNode(resultado);
+    salida.appendChild(texto);
+    salida.nodeValue=" ";*/
+    salida.value=resultado;
+    console.log(resultado);
 }
-var resultado = precio(20000,19);
-console.log(resultado);
